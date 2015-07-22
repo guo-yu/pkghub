585bc47b29abc4c0732014038c748e0c6a014718
Guo Yu <i@guoyu.me>
2015-07-22 16:03:26 +0800

    Update dependencies

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,22 @@
-// 执行各个功能点测试
-var should = require('should'),
-    sys = require('../package.json'),
-    Hub = require('../index'),
-    hub = new Hub;
+var should = require('should')
+var sys = require('../package.json')
+var Hub = require('../index')
+var hub = new Hub
 
 describe('#List', function() {
-    it('should return a module list', function(done) {
-        hub.list(function(err, modules){
-            if (err) return console.log(err);
-            // 无法 parse 依赖模块？
-            console.log(modules.dependencies)
-            modules.should.be.an.Object;
-            modules.name.should.equal(sys.name);
-            modules.version.should.equal(sys.version);
-            modules.dependencies.should.have.property('underscore');
-            modules.dependencies.underscore.should.not.be.empty;
-            done(err);
-        });
-    });
-});
+  it('should return a module list', function(done) {
+    hub.list(function(err, modules) {
+      if (err) 
+        return console.log(err)
+
+      // 无法 parse 依赖模块？
+      modules.should.be.an.Object
+      modules.name.should.equal(sys.name)
+      modules.version.should.equal(sys.version)
+      modules.dependencies.should.have.property('underscore')
+      modules.dependencies.underscore.should.not.be.empty
+
+      done(err)
+    })
+  })
+})
